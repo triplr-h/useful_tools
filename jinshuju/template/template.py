430d98243dc4a73b26cfb47b07370a2eaeb8e8ba
triplr-h <hhh2738@sjtu.edu.cn>
2020-02-14 13:55:36 +0800

    设置一个金数据模板

---
+++ jinshuju/template/template.py
@@ -0,0 +1,10 @@
+# 为金数据问卷设置一个模板，里面有常用的函数
+# coding:utf-8
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from collections import defaultdict
+from selenium import webdriver
+import pandas as pd
+import datetime
+
